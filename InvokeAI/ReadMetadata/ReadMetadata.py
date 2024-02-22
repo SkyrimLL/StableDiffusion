b550aeedf3b6c59a33aa0cb48a704670c3bbc305
SkyrimLL <deepbluefrog@protonmail.com>
2024-02-22 10:24:56 -0500

    Improved display of metadata from an image

--- InvokeAI/ReadMetadata/ReadMetadata.py
+++ InvokeAI/ReadMetadata/ReadMetadata.py
@@ -2,10 +2,13 @@ import json
 import pprint
 from PIL import Image
 
-filename = 'E:\\Documents\\GenerativeAI\\InvokeAI-2\\_SANDBOX_20230823\\000137.9213cf41.3936649406.png'
+filename = 'E:\\Documents\\GenerativeAI\\InvokeAI-3.3.4\\images\\0c6e74a2-aa10-47cc-a1ef-6f9233b2ad43.png'
 im = Image.open(filename)
 im.load()  # Needed only for .png EXIF data (see citation above)
+
+image_metadata = json.loads(im.info["invokeai_metadata"])
  
-print(json.dumps(im.info, sort_keys=True, indent=4)) 
+print(f">> Decoding metadata for: {filename}")   
+print(json.dumps(image_metadata, sort_keys=True, indent=4)) 
 # pprint.pprint(im.info)
  
